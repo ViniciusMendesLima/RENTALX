8f76fe47926fb9bd8e2e6b63b889545905b098b0
ViniciusMendesLima <viniciusmendeslima@yahoo.com>
2021-09-07 09:52:40 -0300

    first commit

---
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -0,0 +1,15 @@
+import { Specification } from "../model/Specification";
+import { ICategoriesRepository } from "./ICategoriesRepository"
+
+interface ICreateSpecificationDTO{
+  name: string;
+  description: string;
+}
+
+interface ISpecificationsRepository{
+
+  create({description, name}: ICreateSpecificationDTO): void;
+  findByName(name: string): Specification;
+}
+
+export {ISpecificationsRepository, ICreateSpecificationDTO};
