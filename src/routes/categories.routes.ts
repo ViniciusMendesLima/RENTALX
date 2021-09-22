5d4ec1c44c5f6d67e7a17b58be0d1acc70a35934
ViniciusMendesLima <viniciusmendeslima@yahoo.com>
2021-09-22 18:48:48 -0300

    atualizar

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import  multer  from "multer";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { importCategoryController } from "../modules/cars/useCases/importCategory";
@@ -8,20 +8,19 @@ import { listCategoriesController } from "../modules/cars/useCases/listCategorie
 const categoriesRoutes = Router();
 
 const upload = multer({
-  dest:"./tmp",
+  dest: "./tmp",
 });
 
-categoriesRoutes.post("/", (request, response) =>{
- return createCategoryController.handle(request, response);
-});
+categoriesRoutes.post("/", (request, response) =>
+  createCategoryController.handle(request, response)
+);
 
-categoriesRoutes.get("/", (request, response) => {
-return listCategoriesController.handle(request, response);
-});
+categoriesRoutes.get("/", (request, response) =>
+  listCategoriesController.handle(request, response)
+);
 
-categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
-  return importCategoryController.handle(request, response);
+categoriesRoutes.post("/import", upload.single("file"), (request, response) =>
+  importCategoryController.handle(request, response)
+);
 
-});
-
-export {categoriesRoutes};
+export { categoriesRoutes };
